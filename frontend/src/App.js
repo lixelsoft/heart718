89061980957a3e78757a658ac4d66a03c1922692
mchann <mch4nn@gmail.com>
2020-03-30 12:02:38 +0900

    Init Repo

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PopUp from './PopUp'
+function App() {
+  return (
+    <div className="App">
+      <PopUp />
+    </div>
+  );
+}
+
+export default App;
