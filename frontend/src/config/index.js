c007fd467923efb9cc005d518e3d1cb961256d6d
mchann <mch4nn@gmail.com>
2020-08-03 14:25:39 +0900

    Add Daily Revenue

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -6,7 +6,7 @@ module.exports = {
   // API_URL : "http://localhost:3100/api/",
   
   // from external
-  URL : "http://3.34.48.162:8719/api",
+  URL : "http://52.78.155.39:3200/api",
   // LOCALHOST: "http://localhost:8719/api",
 
   
